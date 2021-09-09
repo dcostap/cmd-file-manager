e59fa5f6fef363b5303a746827bf38d6499eca22
Darío Costa <dariocostapena@gmail.com>
2021-09-09 09:20:33 +0200

    Big re-structure

--- main.py
+++ main.py
@@ -5,36 +5,7 @@ import os
 from pathlib import Path
 import threading
 import time
-
-lines: list[str] = []
-line_paths: list[Path] = []
-
-close_application = False
-
-class Cursor():
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        pass
-
-    def ensure_in_bounds(self):
-        assert len(lines) > 0, "Empty lines!"
-
-        if self.y >= len(lines):
-            self.y = len(lines) - 1
-        elif self.y < 0:
-            self.y = 0
-
-        current_line = lines[self.y]
-
-        if self.x > len(current_line):
-            self.x = len(current_line)
-        elif self.x < 0:
-            self.x = 0
-
-cursor: Cursor = Cursor()
-
-window: curses.window = None
+from windows import *
 
 def get_width():
     return curses.COLS
@@ -42,111 +13,22 @@ def get_width():
 def get_height():
     return curses.LINES
 
-def refresh():
-    window.clear()
-    y = 0
-    for line in lines:
-        window.addstr(y, 0, line)
-        y += 1
+def main(main_window):
+    close_application = False
 
-    cursor.ensure_in_bounds()
-    window.chgat(cursor.y, cursor.x, 1, curses.A_BLINK)
-    window.refresh()
+    cursor: Cursor = Cursor()
 
-def process_write_input(input):
-    global close_application
+    main_window.clear()
+    curses.curs_set(0)
+    main_window.nodelay(1)
 
-    cursor.ensure_in_bounds()
-    current_line = lines[cursor.y]
+    filenames_area: FilenamesArea = None
+    filenames_area = FilenamesArea()
+    filenames_area.read_folder(os.getcwd())
 
-    if isinstance(input, int):
-        # arrow keys
-        if input == 258:
-            cursor.y += 1
-        elif input == 259:
-            cursor.y -= 1
-        elif input == 260:
-            cursor.x -= 1
-        elif input == 261:
-            cursor.x += 1
-        # delete
-        elif input == 330:
-            if len(current_line) > cursor.x:
-                current_line_list = list(current_line)
-                del current_line_list[cursor.x]
-                lines[cursor.y] = "".join(current_line_list)
-
-    else:
-        print("input info:\nstring ->{}<-\nbytes ->{}<-".format(str(input), str(bytes(input, 'utf-8'))))
-
-        do_nothing_byte_inputs = [
-            b'\t'
-        ]
-
-        exit_byte_inputs = [
-            b'\x03', b'\x1b'
-        ]
-
-        # pressed escape?
-        if bytes(input, 'utf-8') in exit_byte_inputs:
-            print("closed")
-            close_application = True
-            return
-        elif bytes(input, 'utf-8') == b'\n':
-            cursor.y += 1
-            return
-        elif bytes(input, 'utf-8') in do_nothing_byte_inputs:
-            print("do nothing")
-            return
-        elif bytes(input, 'utf-8') == b'\x08': # backspace
-            if cursor.x == 0:
-                return
-            current_line_list = list(current_line)
-            del current_line_list[cursor.x - 1]
-            lines[cursor.y] = "".join(current_line_list)
-            cursor.x -= 1
-            return
-
-        current_line_list = list(current_line)
-        current_line_list.insert(cursor.x, input)
-
-        lines[cursor.y] = "".join(current_line_list)
-        cursor.x += 1
-
-def read_folder(path: str):
-    global lines
-    global lines_paths
-
-    assert not os.path.isfile(path)
-
-    lines = []
-    line_paths = []
-    for file in os.listdir(path):
-        if not os.path.isfile(file):
-            lines.append("./" + file)
-            line_paths.append(os.path.join(path, file))
-
-    for file in os.listdir(path):
-        if os.path.isfile(file):
-            lines.append(file)
-            line_paths.append(os.path.join(path, file))
-
-    assert len(lines) > 0
-
-def main(new_window):
-    global window
-    window = new_window
-    window.clear()
+    active_area: WindowArea = filenames_area
 
     print("Window size is... w: {}, h: {}".format(str(get_width()), str(get_height())))
-
-    curses.curs_set(0)
-    window.nodelay(1)
-
-    read_folder(os.getcwd())
-
-    cursor.ensure_in_bounds()
-    refresh()
 
     blinking_state = False
 
@@ -156,21 +38,52 @@ def main(new_window):
     # blinking during timeouts
     NORMAL_BLINKING = 500
 
-    window.timeout(SHORT_BLINKING)
+    main_window.timeout(SHORT_BLINKING)
+
+    def refresh_windows():
+        main_window.refresh()
+        filenames_area.refresh(cursor)
+
+    def is_close_input(input):
+        if isinstance(input, int):
+            return False
+
+        exit_byte_inputs = [
+            b'\x03', b'\x1b'
+        ]
+        return bytes(input, 'utf-8') in exit_byte_inputs
+
+    def resize_windows():
+        filenames_area.display_x = 0
+        filenames_area.display_y = 0
+        filenames_area.display_width = int(get_width() / 2)
+        filenames_area.display_height = get_height()
+
+    resize_windows()
+
     while not close_application:
         try:
-            char = window.get_wch()
-            process_write_input(char)
-            refresh()
+            input = main_window.get_wch()
+
+            if is_close_input(input):
+                print("closed")
+                close_application = True
+                break
+
+            cursor.ensure_in_bounds(active_area)
+            active_area.process_write_input(cursor, input)
+            cursor.ensure_in_bounds(active_area)
+            refresh_windows()
             blinking_state = False
-            window.timeout(SHORT_BLINKING)
+            main_window.timeout(SHORT_BLINKING)
         except curses.error: # TODO narrow it down so I only catch the timeout error
             # timeout while waiting for user input
-            cursor.ensure_in_bounds()
-            window.chgat(cursor.y, cursor.x, 1, curses.A_NORMAL if not blinking_state else curses.A_BLINK)
-            window.refresh()
+            refresh_windows()
+            cursor.ensure_in_bounds(active_area)
+            if len(active_area.lines) > 0:
+                active_area.pad.chgat(cursor.y, cursor.x, 1, curses.A_NORMAL if not blinking_state else curses.A_BLINK)
             blinking_state = not blinking_state
-            window.timeout(NORMAL_BLINKING)
+            main_window.timeout(NORMAL_BLINKING)
 
 if __name__ == '__main__':
     curses.wrapper(main)
---
+++ windows.py
@@ -0,0 +1,140 @@
+import curses
+import curses.textpad
+import curses.ascii
+import os
+
+class WindowArea():
+    def __init__(self):
+        self.pad = curses.newpad(0, 0)
+        self.scroll_x = 0
+        self.scroll_y = 0
+        self.lines: list[str] = []
+
+        self.display_width = 0
+        self.display_height = 0
+        self.display_x = 0
+        self.display_y = 0
+
+    def get_width(self):
+        longest_line = max([len(line) for line in self.lines])
+        return longest_line + 1 # buffer of 1 character in the right so cursor can be positioned there
+
+    def get_height(self):
+        return len(self.lines)
+
+
+class FilenamesArea(WindowArea):
+    def __init__(self):
+        super().__init__()
+
+        self.line_paths: list[str] = []
+
+    def refresh(self, cursor):
+        self.pad.clear()
+        if len(self.lines) == 0:
+            return
+
+
+        self.pad.resize(self.get_height(), self.get_width())
+
+        y = 0
+        for line in self.lines:
+            self.pad.addstr(y, 0, line)
+            y += 1
+
+        self.pad.chgat(cursor.y, cursor.x, 1, curses.A_BLINK)
+        self.pad.refresh(self.scroll_y, self.scroll_x, self.display_y, self.display_x,
+                min(self.display_height, self.get_height()),
+                min(self.display_width, self.get_width()))
+
+    def read_folder(self, path: str):
+        assert not os.path.isfile(path)
+
+        self.lines = []
+        self.line_paths = []
+        for file in os.listdir(path):
+            if not os.path.isfile(file):
+                self.lines.append("./" + file)
+                self.line_paths.append(os.path.join(path, file))
+
+        for file in os.listdir(path):
+            if os.path.isfile(file):
+                self.lines.append(file)
+                self.line_paths.append(os.path.join(path, file))
+
+        assert len(self.lines) > 0
+
+    def process_write_input(self, cursor, input) -> bool:
+        if len(self.lines) == 0:
+            return
+
+        current_line = self.lines[cursor.y]
+
+        if isinstance(input, int):
+            # arrow keys
+            if input == 258:
+                cursor.y += 1
+            elif input == 259:
+                cursor.y -= 1
+            elif input == 260:
+                cursor.x -= 1
+            elif input == 261:
+                cursor.x += 1
+            # delete
+            elif input == 330:
+                if len(current_line) > cursor.x:
+                    current_line_list = list(current_line)
+                    del current_line_list[cursor.x]
+                    self.lines[cursor.y] = "".join(current_line_list)
+        else:
+            print("input info:\nstring ->{}<-\nbytes ->{}<-".format(str(input), str(bytes(input, 'utf-8'))))
+
+            do_nothing_byte_inputs = [
+                b'\t'
+            ]
+
+            if bytes(input, 'utf-8') == b'\n':
+                cursor.y += 1
+                return False
+            elif bytes(input, 'utf-8') in do_nothing_byte_inputs:
+                print("do nothing")
+                return False
+            elif bytes(input, 'utf-8') == b'\x08': # backspace
+                if cursor.x == 0:
+                    return False
+                current_line_list = list(current_line)
+                del current_line_list[cursor.x - 1]
+                self.lines[cursor.y] = "".join(current_line_list)
+                cursor.x -= 1
+                return False
+
+            current_line_list = list(current_line)
+            current_line_list.insert(cursor.x, input)
+
+            self.lines[cursor.y] = "".join(current_line_list)
+            cursor.x += 1
+
+        return False
+
+class Cursor():
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        pass
+
+    def ensure_in_bounds(self, window: WindowArea):
+        # assert len(window.lines) > 0, "Empty lines!"
+        if len(window.lines) == 0:
+            return
+
+        if self.y >= len(window.lines):
+            self.y = len(window.lines) - 1
+        elif self.y < 0:
+            self.y = 0
+
+        current_line = window.lines[self.y]
+
+        if self.x > len(current_line):
+            self.x = len(current_line)
+        elif self.x < 0:
+            self.x = 0
